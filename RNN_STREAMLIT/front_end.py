9ca2072162ffa7c73daf9d3a50a3870241cf4aa0
Miguel Rodriguez <miguel.rmorell@gmail.com>
2023-06-14 13:35:05 +0200

    images

--- RNN_STREAMLIT/front_end.py
+++ RNN_STREAMLIT/front_end.py
@@ -25,19 +25,19 @@ with col1:
 with col2:
     # Texto más pequeño con estilo personalizado
     st.markdown("""
-    <div style='font-size: 23px; width: 300px; height: 100px; display: flex; align-items: center; justify-content: center; text-align: center; border: 1px solid black;'>
+    <div style='font-size: 23px; width: 300px; height: 150px; display: flex; align-items: center; justify-content: center; text-align: center; border: 1px solid black;'>
         <b>Need some inspiration for your music creation?</b>
     </div>
     """, unsafe_allow_html=True)
 
 st.markdown("""
-    <div style='font-size: 23px; width: 300px; height: 100px; display: flex; align-items: center; justify-content: center; text-align: center; border: 1px solid black;'>
+    <div style='font-size: 23px; width: 300px; height: 150px; display: flex; align-items: center; justify-content: center; text-align: center; border: 1px solid black;'>
         <b>Need a companion to spice up your drum grooves?</b>
     </div>
     """, unsafe_allow_html=True)
 
 st.markdown("""
-    <div style='font-size: 23px; width: 300px; height: 100px; display: flex; align-items: center; justify-content: center; text-align: center; border: 1px solid black;'>
+    <div style='font-size: 23px; width: 300px; height: 150px; display: flex; align-items: center; justify-content: center; text-align: center; border: 1px solid black;'>
         <b>Sick of your current bass player and want to look for a replacement?</b>
     </div>
     """, unsafe_allow_html=True)
