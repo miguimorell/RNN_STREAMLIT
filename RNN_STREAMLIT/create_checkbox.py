30108316aa3146c17d1ff6d421aff2b9ef342036
Miguel Rodriguez <miguel.rmorell@gmail.com>
2023-06-14 21:26:08 +0200

    images

--- RNN_STREAMLIT/create_checkbox.py
+++ RNN_STREAMLIT/create_checkbox.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-def create_checkbox(text,key):
-    #List of sounds, block of boxes for the two groups of 16th notes.
+import streamlit as st
+
+def create_checkbox(text, key):
+    # List of sounds, block of boxes for the two groups of 16th notes.
 
     # List of sounds for block
     sounds = {
@@ -10,7 +12,7 @@ def create_checkbox(text,key):
         'Hi-Hat': []
     }
 
-    contador = ["1", "e", "Y", "a", "2", "e", "Y", "a", "3", "e", "Y", "a", "4", "e", "Y", "a"]
+    contador = ["1", "e", "&", "a", "2", "e", "&", "a", "3", "e", "&", "a", "4", "e", "&", "a"]
 
     # First block of boxes
     st.write(text)
@@ -19,8 +21,11 @@ def create_checkbox(text,key):
         cols = st.columns(16)
 
         for i, col in enumerate(cols):
-
-            checkbox = col.checkbox(f' {contador[i]}', key="".join([str(i)+key, sound_name]))
+            if contador[i].isdigit():
+                col.markdown(f"<p style='color:red'>{contador[i]}</p>", unsafe_allow_html=True)
+            else:
+                col.write(contador[i])
+            checkbox = col.checkbox(f' {contador[i]}', key="".join([str(i) + key, sound_name]))
             sound_list.append(checkbox)
 
     return sounds
