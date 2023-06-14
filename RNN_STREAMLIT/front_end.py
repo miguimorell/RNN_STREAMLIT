481bcd5a718c084647f012b7ad5ba244ca48d785
Miguel Rodriguez <miguel.rmorell@gmail.com>
2023-06-14 11:45:10 +0200

    images

--- RNN_STREAMLIT/front_end.py
+++ RNN_STREAMLIT/front_end.py
@@ -22,10 +22,10 @@ from call_api import call_api
 
 
 # ADD IMAGE LATERAL LEFT
-st.sidebar.image('/home/miguimorell/code/miguimorell/RNN_STREAMLIT/RNN_STREAMLIT/Images/image1.png', use_column_width=True)
+st.sidebar.image('~/RNN_STREAMLIT/RNN_STREAMLIT/Images/image1.png', use_column_width=True)
 
 # ADD IMAGE LATERAL RIGHT
-st.sidebar.image('/home/miguimorell/code/miguimorell/RNN_STREAMLIT/RNN_STREAMLIT/Images/image1.png', use_column_width=True)
+st.sidebar.image('~/RNN_STREAMLIT/RNN_STREAMLIT/Images/image1.png', use_column_width=True)
 
 
 
