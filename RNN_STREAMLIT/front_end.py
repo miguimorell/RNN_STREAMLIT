3f3565ec0a0f1d44f068b021edb09fad263fe864
Miguel Rodriguez <miguel.rmorell@gmail.com>
2023-06-14 12:54:18 +0200

    images

--- RNN_STREAMLIT/front_end.py
+++ RNN_STREAMLIT/front_end.py
@@ -12,7 +12,7 @@ url_logo ='https://st2.depositphotos.com/15317184/46760/v/600/depositphotos_4676
 st.image(url_logo, width=600)
 
 
-col1,col2,col3=st.columns(3)
+col1,col2=st.columns(3)
 
 with col1:
     # Mostrar un GIF desde una URL
@@ -33,55 +33,55 @@ with col2:
 
 
 
-    #List of sounds, block of boxes for the two groups of 16th notes.
-    sounds_first_block = create_checkbox("Enter the drums for the first bar:","1")
-    st.write("---")
-    sounds_second_block = create_checkbox("Enter the drums for the second bar:","2")
-    st.write("---")
-    #Botton to specify temperature
-    st.session_state['temperature']=insert_temperature()
-    st.write("---")
+#List of sounds, block of boxes for the two groups of 16th notes.
+sounds_first_block = create_checkbox("Enter the drums for the first bar:","1")
+st.write("---")
+sounds_second_block = create_checkbox("Enter the drums for the second bar:","2")
+st.write("---")
+#Botton to specify temperature
+st.session_state['temperature']=insert_temperature()
+st.write("---")
 
-    title = st.text_input('Insert File Name', 'Bass')
+title = st.text_input('Insert File Name', 'Bass')
 
-    # Botón Submit
-    if st.button("Create Bass Sound"):
+# Botón Submit
+if st.button("Create Bass Sound"):
 
-        # Almacenar los resultados seleccionados en una lista
-        # Primer bloque de cajas
-        kick_data,snare_data,charles_data = check_checkbox(sounds_first_block)
+    # Almacenar los resultados seleccionados en una lista
+    # Primer bloque de cajas
+    kick_data,snare_data,charles_data = check_checkbox(sounds_first_block)
 
-        # Segundo bloque de cajas
-        kick_data2,snare_data2,charles_data2 = check_checkbox(sounds_second_block)
+    # Segundo bloque de cajas
+    kick_data2,snare_data2,charles_data2 = check_checkbox(sounds_second_block)
 
-        #Concatenar los outputs en 3 strings.
-        #orden-> Charles, kick, snare
-        #Sumar las listas de los dos bloques
+    #Concatenar los outputs en 3 strings.
+    #orden-> Charles, kick, snare
+    #Sumar las listas de los dos bloques
 
-        kick=kick_data+kick_data2
-        snare=snare_data+snare_data2
-        charles=charles_data+charles_data2
+    kick=kick_data+kick_data2
+    snare=snare_data+snare_data2
+    charles=charles_data+charles_data2
 
-        st.session_state['kick'] = kick
-        st.session_state['snare'] = snare
-        st.session_state['charles'] = charles
+    st.session_state['kick'] = kick
+    st.session_state['snare'] = snare
+    st.session_state['charles'] = charles
 
-        kick_str = ",".join(str(elemento) for elemento in kick)
-        snare_str = ",".join(str(elemento) for elemento in snare)
-        charles_str = ",".join(str(elemento) for elemento in charles)
+    kick_str = ",".join(str(elemento) for elemento in kick)
+    snare_str = ",".join(str(elemento) for elemento in snare)
+    charles_str = ",".join(str(elemento) for elemento in charles)
 
-        #call api
-        query = call_api(charles_str,kick_str,snare_str,st.session_state["temperature"])
+    #call api
+    query = call_api(charles_str,kick_str,snare_str,st.session_state["temperature"])
 
-        bass=[]
-        for key, value in query.items():
-            bass.append(value)
+    bass=[]
+    for key, value in query.items():
+        bass.append(value)
 
-        # Save the melody as a MIDI file
-        file_name = title+'.mid'
-        save_melody2(bass, step_duration=0.25,format='midi', file_name= file_name)
+    # Save the melody as a MIDI file
+    file_name = title+'.mid'
+    save_melody2(bass, step_duration=0.25,format='midi', file_name= file_name)
 
-        # Provide download link for the generated MIDI file
-        audio_file = open(file_name, 'rb')
-        audio_bytes = audio_file.read()
-        st.download_button("Download MIDI File", data=audio_bytes, file_name=file_name)
+    # Provide download link for the generated MIDI file
+    audio_file = open(file_name, 'rb')
+    audio_bytes = audio_file.read()
+    st.download_button("Download MIDI File", data=audio_bytes, file_name=file_name)
