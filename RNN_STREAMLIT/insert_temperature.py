bf8f1dd63360efb6cceb223bdf7cc0c96b12f32d
Miguel Rodriguez <miguel.rmorell@gmail.com>
2023-06-14 13:58:07 +0200

    images

--- RNN_STREAMLIT/insert_temperature.py
+++ RNN_STREAMLIT/insert_temperature.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def insert_temperature():
-    st.write("¨**Temperature**¨ is a parameter that adds randomness to the generation: Numbers closer to 0 make the model more deterministic. Numbers closer to 1 makes the generation more unpredictable.")
+    st.markdown("<span style='font-size:20px'><b>Temperature</b> is a parameter that adds randomness to the generation: Numbers closer to 0 make the model more deterministic. Numbers closer to 1 make the generation more unpredictable.</span>", unsafe_allow_html=True)
     temperature = st.slider('Select a value for the temperature', 0.0, 1.0, step=0.1)
 
     return temperature
