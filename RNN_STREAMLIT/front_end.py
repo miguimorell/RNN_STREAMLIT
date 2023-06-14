0a8369bead60bf72730e4e41c44c909279d4a8f8
Miguel Rodriguez <miguel.rmorell@gmail.com>
2023-06-14 21:10:36 +0200

    images

--- RNN_STREAMLIT/front_end.py
+++ RNN_STREAMLIT/front_end.py
@@ -15,7 +15,7 @@ if "Download_press" not in st.session_state:
     st.session_state["Download_press"] = False
 
 # Mostrar logo
-st.image(url_logo, width=600)
+st.image(url_logo, width=630)
 
 
 col1,col2=st.columns([4,6])
